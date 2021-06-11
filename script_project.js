7bb94523cc77e74b14e7d8313d2c2323b09ebf03
mehmetkonat <82652138+mehmetkonat@users.noreply.github.com>
2021-06-11 15:29:57 +0300

    Add files via upload

--- script_project.js
+++ script_project.js
@@ -1,21 +1,3 @@
-let boxes = document.querySelectorAll("span");
-for (let i = 0, len = boxes.length; i < len; i++) {
-    boxes[i].addEventListener("mouseover", Enlarge, true);
-    boxes[i].addEventListener("mouseout", Shrink, true);
-}
-
-function Enlarge() {
-   this.style.backgroundSize = "100% 100%";
-
-
-}
-
-function Shrink() {
-    this.style.backgroundSize = "0px 0%";
-
- }
-
-
  function myFunction(imgs) {
     var expandImg = document.getElementById("expandedImg");
     var imgText = document.getElementById("imgtext");
