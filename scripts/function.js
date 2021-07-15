8e1b2ed5763f66c800e68cad10eea077bfdabe9d
mehmetkonat <82652138+mehmetkonat@users.noreply.github.com>
2021-07-15 12:50:04 +0300

    Add files via upload

---
+++ scripts/function.js
@@ -0,0 +1,12 @@
+
+var $header_top = $('.header-top');
+var $nav = $('nav');
+ 
+$header_top.find('a').on('click', function() { $(this).parent().toggleClass('open-menu');});
+ 
+$('#').fullpage({
+
+ 
+ 
+});
+
